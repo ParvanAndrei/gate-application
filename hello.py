c8c36e7b0d65770f5f950f488976dfba214e7e08
ParvanAndrei <andrei.parvan81@gmail.com>
2021-09-14 12:44:08 +0300

    change hello word and add template

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask import *
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+#@app.route("/")
+#def hello_world():
+#	app.logger.info('logging successfully')
+#	return render_template("index.html")
 
+@app.route("/", methods=['POST', 'GET'])
+def load():
+	if request.method == "POST":
+		return render_template('test.html')
+	return render_template("index.html")
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0", port=80, debug=True)
